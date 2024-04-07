0fc37e0cf21b9b7dff03c1bbd3ce52f9f8955938
yusif.h <yusif.h@vitanur.com>
2024-04-07 16:41:12 +0400

    suara

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Sep 15, 2022 15:37:25").getTime();
+var countDownDate = new Date("May 31, 2024 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
